21d851f63625c7d31112df5a0017e016016a2411
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-10-07 16:14:02 -0500

    step6 ready

--- src/components/key-people/keyPeople.js
+++ src/components/key-people/keyPeople.js
@@ -14,11 +14,11 @@ const KeyPeople = () => {
             </header>
             <h1 className="keyPeople__title">Key people</h1>
             <section className="keyPeople">
-                <Card rolePeople={"SP & UI STUDIO PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/damian.png"} namePeople={"Damian Lazzarano"} emailPeople={"@damian.lazzarano"} />
-                <Card rolePeople={"SP & UI STUDIO PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/david.png"} namePeople={"David Correa"} emailPeople={"@david.correa"} />
-                <Card rolePeople={"UI ENGINEERING PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/luciana.png"} namePeople={"Luciana Zavaleta"} emailPeople={"@luciana.zavaleta"} />
-                <Card rolePeople={"UI TDC FOCAL TECH. DIRECTOR"} imgPeople={process.env.PUBLIC_URL + "../assets/img/gabriel.png"} namePeople={"Gabriel Martinez"} emailPeople={"@gabo"} />
-                <Card rolePeople={"UI ENGINEERING TECH. MANAGER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/sebastian.png"} namePeople={"Sebastián Gómez"} emailPeople={"@s.gomez"} />
+                <Card rolePeople={"SP & UI STUDIO PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/damian.png"} namePeople={"Damian Lazzarano"} emailRoute={"https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=damian.lazzarano@globant.com"} emailPeople={"@damian.lazzarano"} />
+                <Card rolePeople={"SP & UI STUDIO PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/david.png"} namePeople={"David Correa"} emailRoute={"https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=david.correa@globant.com"} emailPeople={"@david.correa"} />
+                <Card rolePeople={"UI ENGINEERING PARTNER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/luciana.png"} namePeople={"Luciana Zavaleta"} emailRoute={"https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=luciana.zavaleta@globant.com"} emailPeople={"@luciana.zavaleta"} />
+                <Card rolePeople={"UI TDC FOCAL TECH. DIRECTOR"} imgPeople={process.env.PUBLIC_URL + "../assets/img/gabriel.png"} namePeople={"Gabriel Martinez"} emailRoute={"https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=gabriel.martinez@globant.com"} emailPeople={"@gabo"} />
+                <Card rolePeople={"UI ENGINEERING TECH. MANAGER"} imgPeople={process.env.PUBLIC_URL + "../assets/img/sebastian.png"} namePeople={"Sebastián Gómez"} emailRoute={"https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=s.gomez@globant.com"} emailPeople={"@s.gomez"} />
             </section>
             <NextStep myClass="next__btn next" route={`/people`}/>
         </div>
--- src/components/shared/card/card.js
+++ src/components/shared/card/card.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './card.scss';
 
-const Card = ({rolePeople,imgPeople,namePeople,emailPeople}) => {
+const Card = ({rolePeople,imgPeople,namePeople,emailRoute,emailPeople}) => {
     return (
         <div className="card">
             <h3>{rolePeople}</h3>
             <img src={imgPeople} alt={rolePeople}/>
             <p>{namePeople}</p>
-            <button>{emailPeople}</button>
+            <a target="_blanck" href={emailRoute}>{emailPeople}</a>
         </div>
     )
 }
